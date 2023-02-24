52b885f4855ab0916f8969cc8564f7e2f04d1e92
markyMARK <>
2023-02-24 02:13:10 -0500

    Updated component and e2e tests for App

--- cypress/e2e/App.spec.cy.js
+++ cypress/e2e/App.spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test the home page', () => {
   it('visits the app root url', () => {
     cy.visit('http://localhost:3000/')
-    cy.get('.App').should('have.length', 1)
+    cy.get('title').contains('U P P E R C A S E')
   })
 })
--- src/App.cy.js
+++ src/App.cy.js
@@ -2,8 +2,10 @@ import React from 'react'
 import App from './App'
 
 describe('<App />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
-  })
+	it('renders', () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<App />)
+		cy.get('.App').should('exist')
+		cy.get('.App').should('have.length', 1)
+	})
 })
