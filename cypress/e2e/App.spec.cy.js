87a87976c7418fdaf22869e7d473e699b92b4db8
markyMARK <>
2023-02-23 19:20:33 -0500

    http://localhost:3000

--- cypress/e2e/App.spec.cy.js
+++ cypress/e2e/App.spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test the home page', () => {
   it('visits the app root url', () => {
-    cy.visit('/')
+    cy.visit('http://localhost:3000/')
     cy.get('.App').should('have.length', 1)
   })
 })
