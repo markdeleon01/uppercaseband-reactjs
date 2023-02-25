cdbff4e1bdd100f4144d2436ca6c7faf83959229
markyMARK <>
2023-02-24 19:22:26 -0500

    home page, nav bar

--- cypress/e2e/Home.spec.cy.js
+++ cypress/e2e/Home.spec.cy.js
@@ -3,4 +3,11 @@ describe('Test the home page', () => {
 		cy.visit('http://localhost:3000/')
 		cy.get('h1').contains('U P P E R C A S E')
 	})
+
+	it('has navigation links', () => {
+		cy.visit('http://localhost:3000/')
+		cy.get('#nav').should('exist')
+		cy.get('#nav').should('have.length', 1)
+		cy.get('#nav > a').should('have.length', 4)
+	})
 })
